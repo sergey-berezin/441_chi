aebf36f016811cdda84f8c8440f473ea7038e3c9
dracarysq1 <58299047+dracarysq1@users.noreply.github.com>
2021-10-06 19:10:56 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Library;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string imageFolder)
+        {
+           
+                Console.WriteLine("Please type path to the image folder");
+                imageFolder = Console.ReadLine();
+                await Program.Main(imageFolder);
+            
+
+        }
+    }
+}
