805106ca8238b24691b3a1fefe5b75db39a2fcba
dracarysq1 <58299047+dracarysq1@users.noreply.github.com>
2021-12-21 16:32:10 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Library;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace ConsoleApp1
+{
+    class Program1
+    {
+              static async Task ImageRecog()
+        {
+           
+            Console.WriteLine("Write input path: ");
+           
+            string imageFolder = @"C:Images";
+          
+        }
+    }
+}
---
+++ Lab_GUI/Info.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_GUI
+{
+    public class Iofo : IEnumerable<string>
+    {
+        public string Info { get; set; }
+        public List<string> list { get; set; }
+
+        public Iofo(string newType, string newImage)
+        {
+            Info = newType;
+            list = new List<string>();
+            list.Add(newImage);
+        }
+
+        
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return ((IEnumerable<string>)list).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)list).GetEnumerator();
+        }
+    }
+}
