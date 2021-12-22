29faedc8888ba578d7bdbe6608ef64ba8962dbe3
dracarysq1 <58299047+dracarysq1@users.noreply.github.com>
2021-12-22 16:25:22 +0300

    Add files via upload

---
+++ Lab_GUI/Context.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.IO;
+using System.Drawing;
+using System.Collections;
+
+namespace Lab_GUI
+{
+    public class Context
+    {
+        public int ObjectId { get; set; }
+        public float x1 { get; set; }
+        public float y1 { get; set; }
+        public float x2 { get; set; }
+        public float y2 { get; set; }
+        public byte[] Image { get; set; }
+        public DBresult Type { get; set; }
+
+    }
+
+    public class DBresult
+    {
+        public int Rid { get; set; }
+        public string Type { get; set; }
+        public ICollection<Object> Objects { get; set; }
+        public override string ToString()
+        {
+            return Type;
+        }
+    }
+    public class context : DbContext
+    {
+        public DbSet<DBresult> results { get; set; }
+        public DbSet<Object> Object { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder o)
+           => o.UseSqlite(@"Data Source=C:\Users\91930\Desktop\c#\database.db");
+
+        public static byte[] ImageToByte2(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+        public void Add(string type, float[] BBox, Bitmap bitmap)
+        {
+            var db = new Context();
+            var query = results.Where(p => type == p.Type);
+            if (query.Count() > 0)
+            {
+                db.Type = query.First();
+            }
+            else
+            {
+                db.Type = new DBresult();
+                db.Type.Type = type;
+                results.Add(db.Type);
+            }
+            SaveChanges();
+        }
+        public IEnumerable<string> GetTypes()
+        {
+            foreach (var res in results)
+            {
+                yield return res.Type;
+            }
+        }
+        public void Delete(string type)
+        {
+            foreach (var db in Object)
+            {
+
+                Object.Remove(db);
+
+            }
+
+
+            SaveChanges();
+        }
+    }
+}
---
+++ Lab_GUI/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Library;
+using Ookii.Dialogs.Wpf;
+using System.Drawing;
+
+namespace Lab_GUI
+{
+
+
+    public partial class MainWindow : Window
+    {
+        public static context db = new context();
+        public static ObservableCollection<Iofo> result = new ObservableCollection<Iofo>();
+
+        private static async Task main()
+        {
+            string type;
+            string image;
+
+            while (true)
+            {
+                (type, image) = await Program.bufferblock.ReceiveAsync();
+               
+
+                bool flag = true;
+                foreach (Iofo r in result)
+                {
+                    if (r.Info == type)
+                    {
+                        r.list.Add(image);
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    result.Add(new Iofo(type, image));
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = result;
+        }
+
+        private void Select_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
+            if ((bool)dialog.ShowDialog())
+                Path.Text = dialog.SelectedPath;
+        }
+
+        private async void Strat_Click(object sender, RoutedEventArgs e)
+        {
+            Strat.IsEnabled = false;
+            result.Clear();
+            Program.cancelTokenSource = new CancellationTokenSource();
+            Program.cancel = Program.cancelTokenSource.Token;
+
+            await Task.WhenAll(Program.ImageRecog(Path.Text), main());
+            Strat.IsEnabled = true;
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            Program.cancelTokenSource.Cancel();
+            Stop.IsEnabled = true;
+        }
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            db.Delete(listBox_types.SelectedItem.ToString());
+        }
+    }
+}
